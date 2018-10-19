69f9605918366a6b61eeeceabd9dbedfc238054b
Profesor Jean Paul <jean.fuentes@profesor.duoc.cl>
2018-10-19 14:45:34 -0300

    Agrega el uso de post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', { 'posts' : posts })
