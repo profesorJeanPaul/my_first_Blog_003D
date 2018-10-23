9ebee93058d591f1dc50790ee6337aba64af3b5b
Profesor Jean Paul <jean.fuentes@profesor.duoc.cl>
2018-10-23 13:14:57 -0300

    Agrega unit test de ejemplo

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Post
+from django.utils import timezone
 
-# Create your tests here.
+class PostTestCase( TestCase ):
+    def test_postPublish( self ):
+        # Arrange
+        expected = 2
+        result = -1
+        # Act
+        result = 2
+        # Assert
+        self.assertEqual(expected,result)
+
+    def test_postPublish2( self ):
+        # Arrange
+        expected = 2
+        result = -1
+        # Act
+        result = 3
+        # Assert
+        self.assertEqual(expected,result)
+
+    def test_postPublish3( self ):
+        # Arrange
+        expected = 2
+        result = -1
+        # Act
+        result = 2
+        # Assert
+        self.assertEqual(expected,result)
